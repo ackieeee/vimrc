97ee7703549855836ca1f682dc4675ca9cb22f8b
gba <gba@email.com>
2020-01-22 00:48:59 +0900

    vim setting sh file

---
+++ vim_setting.sh
@@ -0,0 +1,6 @@
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/installer.sh
+sh /tmp/installer.sh ~/.vim/dein
+
+git clone git@github.com:gba-3/vimrc.git
+mv ./vimrc/.vimrc ~/.vim
+
